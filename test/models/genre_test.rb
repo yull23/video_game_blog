4108b4baa2b163129b9aa690d61722d4af81809b
Yull Timoteo <yull.timoteo.z@uni.pe>
2023-07-31 11:16:51 -0500

    Adding validations for genre, and corresponding test

--- test/models/genre_test.rb
+++ test/models/genre_test.rb
@@ -1,7 +1,31 @@
 require "test_helper"
 
 class GenreTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Validate correct genres" do
+    # skip
+    genre_1 = genres(:genre_1)
+    assert genre_1.valid?
+    assert_not_nil genre_1.name
+
+    genre_2 = genres(:genre_2)
+    assert genre_2.valid?
+    assert_not_nil genre_2.name
+
+    genre_3 = genres(:genre_3)
+    assert genre_3.valid?
+    assert_not_nil genre_3.name
+  end
+
+  test "Validate that name is required and unique" do
+    skip
+    # Género sin nombre debe ser inválido
+    genre = Genre.new
+    assert_not genre.valid?
+    assert_includes genre.errors.full_messages, "Name can't be blank"
+
+    # Género con nombre duplicado debe ser inválido
+    genre_duplicate = Genre.new(name: "Action")
+    assert_not genre_duplicate.valid?
+    assert_includes genre_duplicate.errors.full_messages, "Name has already been taken"
+  end
 end
